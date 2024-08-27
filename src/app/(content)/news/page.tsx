c5f594b3a99675ed40356c5d6cb2d7810bff5e0e
Aleksandra Gasidło <aleksandra.gasidlo@infrabyte.pl>
2024-08-27 09:35:41 +0200

    Add backend, client-side data fetching

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,11 +1,47 @@
-import { DUMMY_NEWS } from "@/lib/dummy-news";
+"use client";
+
 import { NewsList } from "@/components/news-list/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [error, setError] = useState<string>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+      if (!response.ok) {
+        setError("Something went wrong!");
+        setIsLoading(false);
+      }
+      const data = await response.json();
+      setIsLoading(false);
+      setNews(data);
+    };
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
